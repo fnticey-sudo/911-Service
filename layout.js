5b487a79fa03c77d97f307098ff3dd0ddd0da01e
fnticey-sudo <fnticey@gmail.com>
2025-11-19 16:07:25 +0100

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "911 Service Shop",
+  description: "Modz Shop",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
