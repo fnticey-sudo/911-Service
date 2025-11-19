b8cb4adb542f7b8ce75941502fe2cf4dc40f936f
fnticey-sudo <fnticey@gmail.com>
2025-11-19 17:33:29 +0100

    Delete page.js

--- page.js
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <main style={{ padding: "40px", fontFamily: "Arial", color: "white" }}>
-      <h1>Welkom bij 911-Service Shop</h1>
-      <p>De shop is succesvol geladen 🎉</p>
-    </main>
-  );
-}
